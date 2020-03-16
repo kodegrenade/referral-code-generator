9f8a6d7f44a21f02dd0c96bc73abeffad8687544
Temitope Ayotunde <brhamix@gmail.com>
2020-03-16 12:36:45 +0100

    Bug fixed

--- index.js
+++ index.js
@@ -26,45 +26,47 @@ function refCodeGenerator(username, word_length, num_length, type) {
                     if(type == 'uppercase'){
 
                         // check for random number length
-                        if(num_length < 1){
+                        if(num_length > 1){
                             
                             // generate random number
-                            let code = Math.floor(100000 + Math.random() * 900000)
+                            // Math.floor(100000 + Math.random() * 900000)
+                            let code = Math.floor(Math.pow(10, num_length-1) + Math.random() * 9 * Math.pow(10, num_length-1));
                             
                             // referral code
                             return chunked_name.toUpperCase()+''+code;
                         }else{
-                            return 'number length is required.';                            
+                            return 'random number length is required.';                            
                         }
                     }else if(type == 'lowercase'){
                         
                         // check for random number length
-                        if(num_length < 1){
+                        if(num_length > 1){
                             
                             // generate random number
-                            let code = Math.floor(100000 + Math.random() * 900000)
+                            let code = Math.floor(Math.pow(10, num_length-1) + Math.random() * 9 * Math.pow(10, num_length-1));
                             
                             // referral code
-                            return chunked_name.toUpperCase()+''+code;
+                            return chunked_name.toLowerCase()+''+code;
                         }else{
-                            return 'number length is required.';                            
+                            return 'random number length is required.';                            
                         }
                     }else{
                         
                         // check for random number length
-                        if(num_length < 1){
+                        if(num_length > 1){
                             
                             // generate random number
-                            let code = Math.floor(100000 + Math.random() * 900000)
+                            let code = Math.floor(Math.pow(10, num_length-1) + Math.random() * 9 * Math.pow(10, num_length-1));
                             
                             // referral code
                             return chunked_name+''+code;
                         }else{
-                            return 'number length is required.';                            
+                            return 'random number length is required.';                            
                         }
                     }
                 }else{
 
+                    // generate random number
                     let code = Math.floor(100000 + Math.random() * 900000)                                               
 
                     // referral code
